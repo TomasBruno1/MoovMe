4b6fc443486dff5a0519c869662711fe5f24d0d6
tefiporacchia <36172375+tefiporacchia@users.noreply.github.com>
2019-10-14 13:49:56 -0300

    Agregado lo de crear lote

--- src/Administrador.java
+++ src/Administrador.java
@@ -18,4 +18,19 @@ public class Administrador extends Usuario {
         Multa multa = new Multa(c);
         c.multa = multa;
     }
+    public ArrayList<Activo> crearActivosParaLote(TipoDeActivo nombre, int cantidad, Zona zona, int precio, int tarifa){
+        ArrayList<Activo> activos = new ArrayList<>();
+        for (int i= 0; i<cantidad; i++){
+            activos.add(new Activo(nombre, zona, precio, tarifa));
+        }return activos;
+    }
+
+    public Lote crearLoteDeCompraDeActivos(String nombreDelLote, TipoDeActivo nombre, int cantidad, Zona zona, int precio, int tarifa){
+        ArrayList<Activo> activos = this.crearActivosParaLote(nombre, cantidad, zona, precio, tarifa);
+        return new Lote(activos, zona, nombreDelLote);
+    }
+
+    public void adjudicarActivoAlCliente(Cliente c, TipoDeActivo tipodeactivo, Lote lote){
+        c.setActivoEnUso(lote.activosDelLote.get(1)); //Codigo a cambiar, solo para probar
+    }
 }
