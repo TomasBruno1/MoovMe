5effe5c70a3ac5033b3b8dae4f299ca5d677ae91
tefiporacchia <36172375+tefiporacchia@users.noreply.github.com>
2019-10-14 13:41:07 -0300

    Prueba

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void Main (String[]Args){
-        Zona zona = new ZonaNorte();
-        Activo auto = new Auto(zona);
-        System.out.println(auto.tarifaMin);
+         Zona zona = new ZonaNorte();
 
+        Administrador admin = new Administrador();
+
+        TipoDeActivo auto = new TipoDeActivo("auto");
+
+        Lote lote = admin.crearLoteDeCompraDeActivos("Lote De Zona Sur", auto, 3, new ZonaNorte(),1000,10 );
+
+        Cliente tefi = new Cliente("Tefi");
+
+        int codigo = lote.activosDelLote.get(0).getCodigo();
+
+        tefi.usarActivo(auto);
+
+        admin.adjudicarActivoAlCliente(tefi, auto, lote);
+
+        if(!tefi.getActivoEnUso().estaEnZona()){
+            admin.multarCliente(tefi);
+            admin.bloquearCliente(tefi);
+        }
         
     }
 }
